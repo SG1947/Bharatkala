d422c4ca377717c24fb6c8ed512fedc2ef1d0f1d
SG1947 <ghoshsoumili03@gmail.com>
2024-03-17 19:56:07 +0530

    first commit

---
+++ client/src/components/Prices.js
@@ -0,0 +1,32 @@
+export const Prices = [
+    {
+      _id: 0,
+      name: "$50 to 99",
+      array: [50, 99],
+    },
+    {
+      _id: 1,
+      name: "$100 to 149",
+      array: [100, 149],
+    },
+    {
+      _id: 2,
+      name: "$150 to 199",
+      array: [150, 199],
+    },
+    {
+      _id: 3,
+      name: "$200 to 299",
+      array: [200, 299],
+    },
+    {
+      _id: 4,
+      name: "$300 to 499",
+      array: [300, 499],
+    },
+    {
+      _id: 4,
+      name: "$500 or more",
+      array: [500, 9999],
+    },
+  ];
